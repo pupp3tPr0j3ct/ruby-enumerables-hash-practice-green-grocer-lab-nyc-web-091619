9466ddef96c9e56c1e09ce065816e8e0bd3b37c2
Jason Santana <jsantana006@gmail.com>
2019-09-12 17:51:39 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,27 @@
 def consolidate_cart(cart)
-  # code here
+ final_hash = {}
+ cart.each do |element_hash|
+   element_name = element_hash.keys[0]
+   element_stats = element_hash.values[0]
+   
+   if final_hash.has_key?(element_name)
+      final_hash[element_name][:count] += 1
+   else
+      final_hash[element_name] = {
+        count: 1,
+        price: element_stats[:price],
+        clearance: element_stats[:clearance]
+      }
+   end
+  end
+  final_hash
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each do |coupon|
+    item = coupon[:item]
+    if cart[:item]
+      if cart[:item] &&
 end
 
 def apply_clearance(cart)
