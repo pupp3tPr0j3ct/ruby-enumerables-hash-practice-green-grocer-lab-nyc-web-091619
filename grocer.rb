e58298697b37e6778f2e82b025af17cafdb67679
Jason Santana <jsantana006@gmail.com>
2019-09-12 18:40:37 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -22,10 +22,10 @@ def apply_coupons(cart, coupons)
    if cart.keys.include? coupon[:item]
      if cart[coupon[:item][:count] >= coupon[:num]
        new_name = "#{coupon[:item]} W/COUPON"
-       if cart[new_game]
-         cart[new_game][:count] += coupon[:num]
+       if cart[new_name]
+         cart[new_name][:count] += coupon[:num]
        else
-         cart[new_game] = {
+         cart[new_name] = {
            count: coupon[:num],
            price: coupon[:cost]/coupon[:num],
            clearance: cart[coupon[:item]][:clearance]
